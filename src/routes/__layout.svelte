eb246530ba3c9102fc96d659657a4c04303f444a
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2021-07-09 16:16:13 +1000

    working Tailwind setup

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,24 @@
+<script>
+    import '../main.js';
+    import Footer from '$lib/Footer.svelte';
+    import Nav from '$lib/Nav.svelte';
+</script>
+
+<svelte:head>
+    <link rel="icon" href="favicon.svg" />
+    <link rel="mask-icon" color="#f97316" href="mask-icon.svg" />
+    <meta charset="utf-8" />
+    <meta name="description" content="VicEye & Retina Specialists: Cataract and retinal surgeons serving greater Melbourne. Located in Ringwood, Victoria.">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>VicEye Specialist Surgeons</title>
+</svelte:head>
+
+<div class="bg-purple-400">sample div</div>
+
+<Nav />
+
+<main>
+    <slot />
+</main>
+
+<Footer />

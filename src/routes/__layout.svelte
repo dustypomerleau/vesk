74301dbf6536726d7cd975f62e91c27d7e603798
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2021-07-10 11:38:28 +1000

    add favicon and mask-icon

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,8 +5,8 @@
 </script>
 
 <svelte:head>
-    <link rel="icon" href="favicon.svg" />
-    <link rel="mask-icon" color="#f97316" href="mask-icon.svg" />
+    <link rel="icon" href="/favicon.svg" />
+    <link rel="mask-icon" color="#f97316" href="/mask-icon.svg" />
     <meta charset="utf-8" />
     <meta name="description" content="VicEye & Retina Specialists: Cataract and retinal surgeons serving greater Melbourne. Located in Ringwood, Victoria.">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">

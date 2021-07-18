6d08169170fb740ee0faafafa290ad2aafa1df1b
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2021-07-18 14:28:25 +1000

    move doctors and points to TS files

---
+++ src/lib/doctors.ts
@@ -0,0 +1,23 @@
+export const doctors = [
+    {
+        name: "Mei Tan",
+        credentials: "MB ChB BAO, PhD, FRCOphth, FRANZCO",
+        specialties: "Medical &amp; surgical retina, cataract",
+        bio: [
+            "Dr Tan received her specialist training in the UK, completing a PhD in gene therapy at University College London and her clinical training at Oxford Eye Hospital. She then completed vitreoretinal fellowships at the Royal Perth Hospital and the Royal Adelaide Hospital before settling in Melbourne.",
+
+            "Dr Tan conducts clinical research in age-related macular degeneration, including a co-investigator role in clinical trials at the Royal Melbourne Hospital. Additional personal research interests include genetic eye disease, diabetic retinopathy, and vitreoretinal surgery.",
+        ],
+        // todo: src: { small: "mht-small.png", med: "mht-med.png"... }
+        src: "mht.png",
+        alt: "Doctor Mei Tan headshot",
+    },
+    {
+        name: "Dusty Pomerleau",
+        credentials: "MD, FRANZCO",
+        specialties: "Medical &amp; surgical retina, cataract, uveitis",
+        bio: ["bio par 1", "bio par 2"],
+        src: "dlp.jpg",
+        alt: "Doctor Dusty Pomerleau headshot",
+    },
+];

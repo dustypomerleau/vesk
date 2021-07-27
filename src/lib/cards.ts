7f4168790a06b122a68b20e1c6a55427ed45a198
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2021-07-27 17:24:56 +1000

    rename points to cards and include all variables in each object

---
+++ src/lib/cards.ts
@@ -0,0 +1,54 @@
+export const cards = [
+    {
+        title: "Expertise and experience",
+        points: [
+            "Our specialist surgeons are highly qualified and skilled. We bring subspecialty fellowship training and years of clinical experience to every patient interaction.",
+
+            "We offer the latest in laser, surgical, and medical therapies, customised to your needs. Our diligence and careful investigations ensure the most accurate diagnosis.",
+
+            'Our specialists are fellows of the Royal Australian and New Zealand College of Ophthalmologists (<a href="https://ranzco.edu/" class="link">FRANZCO</a>).',
+        ],
+        icon: Eye,
+        buttonText: "Meet our surgeons",
+        href: "/#doctors",
+    },
+    {
+        title: "Trusted by referrers",
+        points: [
+            "Referring doctors deserve timely communication and respectful collaboration. We keep the patient's entire care team connected and informed.",
+
+            "We care for the full spectrum of ophthalmic conditions, including complex vitreoretinal diseases, medical retina, and uveitis.",
+
+            "Our approach to cataract surgery is data-driven and patient-centred. We choose intraocular lenses that maximise individual quality of life.",
+
+            "We accept emergency patients and same-day referrals. Our doctors are always available to speak with you about high-acuity patients.",
+        ],
+        icon: Userplus,
+        buttonText: "Make a referral",
+        href: "/refer",
+    },
+    {
+        title: "Convenient location",
+        points: [
+            "Our Ringwood practice has abundant parking, close to the M3, Ringwood Train Station, and Eastland Shopping Centre.",
+
+            'Our ground-floor office is accessible to wheelchairs and walkers. Feel free to <a href="/#contact" class="link">contact us</a> in advance if you require any special assistance for your visit.',
+
+            "Click the button below for detailed information on how to visit us.",
+        ],
+        icon: Pin,
+        buttonText: "Location and parking",
+        href: "/#location",
+    },
+    {
+        title: "More information",
+        points: [
+            'If you would like to request an appointment—or more information—please <a href="#contact" class="link">reach out</a>. Our team will be in touch to arrange a consultation.',
+
+            "Referrals are required for most of our services. If you have questions about the process, or need help obtaining a referral, we are happy to assist.",
+        ],
+        icon: Mail,
+        buttonText: "Contact us",
+        href: "/#contact",
+    },
+];

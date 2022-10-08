01799951b3de9709b0f5ec492e3bb36bd80ce0c9
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2022-10-08 16:46:13 +1100

    remove excess color from footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -12,14 +12,14 @@
     id="footer"
     class="flex flex-col lg:flex-row lg:justify-center lg:items-center gap-6 lg:gap-12 p-8 sm:px-12 bg-gray-300"
 >
-    <div class="font-normal text-orange-700">
+    <div class="font-normal text-gray-500">
         &copy;2021 Vic&nbsp;Eye&nbsp;and&nbsp;Retina&nbsp;Specialists
         Pty&nbsp;Ltd
     </div>
     {#each links as link}
         <a href={link.href}>
             <div class="lg:text-center">
-                <span class="link">{link.text}</span>
+                <span class="link text-gray-500">{link.text}</span>
             </div>
         </a>
     {/each}

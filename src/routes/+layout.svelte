fe731ddc60d87732b01d69ee289257bfb6f54283
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2022-10-22 16:34:16 +1100

    icon overhaul for safari

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,11 @@
 </script>
 
 <svelte:head>
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    <link rel="icon" href="/favicon.ico" sizes="any" />
+    <link rel="icon" href="/icon.svg" type="image/svg+xml" />
     <link rel="manifest" href="/manifest.json" />
+    <link rel="mask-icon" href="/mask-icon.svg" color="#1785be" />
     <meta charset="utf-8" />
     <meta
         name="description"

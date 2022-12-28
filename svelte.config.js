6b6da7fe11ae420e96d948dc1e19662e0ca8aa7a
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2022-12-28 20:23:17 +1100

    move prerender inside kit settings

--- svelte.config.js
+++ svelte.config.js
@@ -6,15 +6,17 @@ const config = {
     // Consult https://github.com/sveltejs/svelte-preprocess
     // for more information about preprocessors
     preprocess: [preprocess({ postcss: true })],
-    // added prerender settings per this github issue re: netlify forms:
-    // https://github.com/sveltejs/kit/issues/942
-    prerender: {
-        crawl: true,
-        enabled: true,
-        onError: "continue",
-        entries: ["*"]
-    },
-    kit: { adapter: adapter() }
+    kit: {
+        adapter: adapter(),
+        // added prerender settings per this github issue re: netlify forms:
+        // https://github.com/sveltejs/kit/issues/942
+        prerender: {
+            crawl: true,
+            enabled: true,
+            onError: "continue",
+            entries: ["*"]
+        }
+    }
 };
 
 export default config;

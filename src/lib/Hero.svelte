3dc5a171f561379df7e0c3fa429c5537e6c6de64
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-01-02 18:36:31 +0900

    remove fixed background because it fails on Safari

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,6 +1,6 @@
 <div
     id="hero"
-    class="flex h-screen flex-row items-start justify-center bg-logoNavy bg-hero bg-cover bg-top bg-fixed bg-blend-multiply"
+    class="flex h-screen flex-row items-start justify-center bg-logoNavy bg-hero bg-cover bg-top bg-blend-multiply"
 >
     <h1
         class="mt-32 w-screen px-7 text-center text-6xl text-white md:mt-60 md:px-32 lg:w-2/3"

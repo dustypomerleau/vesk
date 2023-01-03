998ed931c415499878bf891a90609d5dab500177
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-01-03 15:52:54 +0900

    change to pure webp background image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -71,7 +71,7 @@ const config = {
         },
         extend: {
             backgroundImage: (theme) => ({
-                hero: "url('/images/hero-50.jpg');",
+                hero: 'url("/images/hero-50.webp");',
                 check: 'url("data:image/svg+xml,%3Csvg%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%20width%3D%2724%27%20height%3D%2724%27%20fill%3D%27none%27%3E%3Cpath%20stroke%3D%27%232e63a7%27%20stroke-linecap%3D%27round%27%20stroke-linejoin%3D%27round%27%20stroke-width%3D%271.5%27%20d%3D%27m5.75%2012.867%202.59%203.547a2%202%200%200%200%203.26-.043l6.65-9.621%27%2F%3E%3C%2Fsvg%3E");'
             }),
             boxShadow: {

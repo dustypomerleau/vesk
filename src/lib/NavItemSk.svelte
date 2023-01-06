4d214294e8cf5db547f69fc5f27faf857753c242
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-01-06 15:28:22 +0900

    reference nav implementation from site-kit

---
+++ src/lib/NavItemSk.svelte
@@ -0,0 +1,37 @@
+<script>
+	/** @type {string} */
+	export let href;
+
+	/** @type {string} */
+	export let external;
+
+	/** @type {string} */
+	export let title;
+
+	export let selected = undefined;
+</script>
+
+{#if external}
+	<li><a href={external} {title} rel="external"><slot /></a></li>
+{:else}
+	<li>
+		<a aria-current={selected} {href} {title}>
+			<slot />
+		</a>
+	</li>
+{/if}
+
+<style>
+	a {
+		font-size: var(--sk-text-s);
+	}
+
+	a:hover {
+		color: var(--sk-theme-3);
+		opacity: 1;
+	}
+
+	[aria-current] {
+		color: var(--sk-theme-1);
+	}
+</style>
--- src/lib/NavSk.svelte
+++ src/lib/NavSk.svelte
@@ -1,270 +1,250 @@
-<!-- forked from https://github.com/sveltejs/site-kit/blob/master/components/Nav.svelte -->
 <script>
-    import { onMount, setContext } from "svelte";
-    import { writable } from "svelte/store";
-    import Icon from "./Icon.svelte";
+	import Icon from './Icon.svelte';
 
-    export let segment;
-    export let page;
-    export let logo;
-    export let home = "Home";
-    export let home_title = "Homepage";
+	export let page;
+	export let logo;
+	export let home = 'Home';
+	export let home_title = 'Homepage';
 
-    const current = writable(null);
-    setContext("nav", current);
+	let open = false;
+	let visible = true;
+	let nav;
 
-    let open = false;
-    let visible = true;
+	// hide nav whenever we navigate
+	page.subscribe(() => {
+		open = false;
+	});
 
-    // hide nav whenever we navigate
-    page.subscribe(() => {
-        open = false;
-    });
+	// Prevents navbar to show/hide when clicking in docs sidebar
+	let hash_changed = false;
+	function handle_hashchange() {
+		hash_changed = true;
+	}
 
-    function intercept_touchstart(event) {
-        if (!open) {
-            event.preventDefault();
-            event.stopPropagation();
-            open = true;
-        }
-    }
+	let last_scroll = 0;
+	function handle_scroll() {
+		const scroll = window.pageYOffset;
+		if (!hash_changed) {
+			visible = scroll < 50 || scroll < last_scroll;
+		}
 
-    // Prevents navbar to show/hide when clicking in docs sidebar
-    let hash_changed = false;
-    function handle_hashchange() {
-        hash_changed = true;
-    }
+		last_scroll = scroll;
+		hash_changed = false;
+	}
 
-    let last_scroll = 0;
-    function handle_scroll() {
-        const scroll = window.pageYOffset;
-        if (!hash_changed) {
-            visible = scroll < 50 || scroll < last_scroll;
-        }
-
-        last_scroll = scroll;
-        hash_changed = false;
-    }
-
-    $: $current = segment;
+	function handle_focus() {
+		if (open && !nav.contains(document.activeElement)) {
+			open = false;
+		}
+	}
 </script>
 
-<svelte:window on:hashchange={handle_hashchange} on:scroll={handle_scroll} />
+<svelte:window
+	on:hashchange={handle_hashchange}
+	on:scroll={handle_scroll}
+	on:focusin={handle_focus}
+/>
 
-<header class:visible={visible || open}>
-    <nav>
-        <a
-            rel="prefetch"
-            href="."
-            class="home"
-            title={home_title}
-            style="background-image: url({logo})">{home}</a
-        >
+{#if open}
+	<div
+		class="modal-background hide-if-desktop"
+		on:click={() => (open = false)}
+	/>
+{/if}
 
-        {#if open}
-            <div
-                class="modal-background hide-if-desktop"
-                on:click={() => (open = false)}
-            />
-        {/if}
+<nav
+	class:visible={visible || open}
+	class:open
+	bind:this={nav}
+	aria-label="Primary"
+>
+	<a
+		href="/"
+		class="nav-spot home"
+		title={home_title}
+		style="background-image: url({logo})"
+	>
+		{home}
+	</a>
 
-        <ul
-            class="primary"
-            class:open
-            on:touchstart|capture={intercept_touchstart}
-            on:mouseenter={() => (open = true)}
-            on:mouseleave={() => (open = false)}
-        >
-            <li class="hide-if-desktop" class:active={!segment}>
-                <a rel="prefetch" href=".">{home}</a>
-            </li>
-            <slot />
-        </ul>
-    </nav>
-</header>
+	<button
+		aria-label="Toggle menu"
+		aria-expanded={open.toString()}
+		class="menu-toggle"
+		class:open
+		on:click={() => (open = !open)}
+	>
+		<Icon name={open ? 'close' : 'menu'} size="1em" />
+	</button>
+
+	<ul>
+		<slot name="nav-center" />
+	</ul>
+
+	<ul class="external">
+		<slot name="nav-right" />
+	</ul>
+</nav>
 
 <style>
-    header {
-        position: fixed;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        width: 100vw;
-        height: var(--nav-h);
-        padding: 0 var(--side-nav);
-        margin: 0 auto;
-        background-color: white;
-        box-shadow: 0 -0.4rem 0.9rem 0.2rem rgba(0, 0, 0, 0.5);
-        font-family: var(--font);
-        z-index: 100;
-        user-select: none;
-        transform: translate(0, calc(-100% - 1rem));
-        transition: transform 0.2s;
-    }
+	.modal-background {
+		position: fixed;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		background: var(--back);
+		opacity: 0.8;
+		z-index: 2;
+		backdrop-filter: grayscale(0.5) blur(2px);
+	}
 
-    header.visible {
-        transform: none;
-    }
+	nav {
+		--shadow-height: 0.5rem;
+		--shadow-gradient: linear-gradient(
+			to bottom,
+			rgba(0, 0, 0, 0.1) 0%,
+			rgba(0, 0, 0, 0.05) 30%,
+			transparent 100%
+		);
+		position: fixed;
+		top: 0;
+		width: 100vw;
+		height: var(--sk-nav-height);
+		margin: 0 auto;
+		background-color: var(--sk-back-2);
+		font-family: var(--sk-font);
+		z-index: 100;
+		user-select: none;
+		transition: transform 0.2s;
+	}
 
-    nav {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100vw;
-        height: var(--nav-h);
-        padding: 0 var(--side-nav) 0 var(--side-nav);
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        background-color: transparent;
-        transform: none;
-        transition: none;
-        box-shadow: none;
-    }
+	nav::after {
+		content: '';
+		position: absolute;
+		width: 100%;
+		height: var(--shadow-height);
+		left: 0;
+		bottom: calc(-1 * var(--shadow-height));
+		background: var(--shadow-gradient);
+	}
 
-    .primary {
-        list-style: none;
-        font-family: var(--font);
-        margin: 0;
-        line-height: 1;
-    }
+	@media (max-width: 800px) {
+		nav:not(.visible):not(:focus-within) {
+			transform: translate(
+				0,
+				calc(-100% - 5rem)
+			); /* TODO revert 5rem to 1rem after we remove the banner */
+		}
+	}
 
-    ul :global(li) {
-        display: block;
-        display: none;
-    }
+	ul {
+		position: relative;
+		width: 100%;
+		padding: 0;
+		margin: 0;
+		list-style: none;
+	}
 
-    ul :global(li).active {
-        display: block;
-    }
+	ul :global(a) {
+		color: var(--sk-text-2);
+	}
 
-    ul {
-        position: relative;
-        padding: 0 3rem 0 0;
-        background: url(/icons/chevron.svg) calc(100% - 1em) 0.05em no-repeat;
-        background-size: 1em 1em;
-    }
+	.home {
+		width: 30rem;
+		height: var(--sk-nav-height);
+		display: flex;
+		text-indent: -9999px;
+		background-position: calc(var(--sk-page-padding-side) - 1rem) 50%;
+		background-repeat: no-repeat;
+		background-size: auto 70%;
+	}
 
-    ul::after {
-        /* prevent clicks from registering if nav is closed */
-        position: absolute;
-        content: "";
-        width: 100%;
-        height: 100%;
-        left: 0;
-        top: 0;
-    }
+	button {
+		position: absolute;
+		top: calc(var(--sk-nav-height) / 2 - 1rem);
+		right: var(--sk-page-padding-side);
+		line-height: 1;
+	}
 
-    ul.open {
-        padding: 0 0 1em 0;
-        background: white;
-        border-left: 1px solid #eee;
-        border-right: 1px solid #eee;
-        border-bottom: 1px solid #eee;
-        border-radius: 0 0 var(--border-r) var(--border-r);
-        align-self: start;
-    }
+	@media (max-width: 800px) {
+		ul {
+			position: relative;
+			display: none;
+			width: 100%;
+			background: var(--back);
+			padding: 1rem var(--sk-page-padding-side);
+		}
 
-    ul.open :global(li) {
-        display: block;
-        text-align: right;
-    }
+		.open ul {
+			display: block;
+		}
 
-    ul.open::after {
-        display: none;
-    }
+		ul.external {
+			padding: 1rem var(--sk-page-padding-side) 1rem;
+		}
 
-    ul :global(li) :global(a) {
-        font-size: var(--h5);
-        padding: 0 0.8rem;
-        border: none;
-        color: inherit;
-    }
+		ul.external::before {
+			content: '';
+			position: absolute;
+			top: 0;
+			left: var(--sk-page-padding-side);
+			width: calc(100% - 2 * var(--sk-page-padding-side));
+			height: 1px;
+			background: radial-gradient(
+				circle at center,
+				rgba(0, 0, 0, 0.1),
+				rgba(0, 0, 0, 0.05)
+			);
+		}
 
-    ul.open :global(li) :global(a) {
-        padding: 1.5rem 3.7rem 1.5rem 4rem;
-        display: block;
-    }
+		ul.external::after {
+			content: '';
+			position: absolute;
+			width: 100%;
+			height: var(--shadow-height);
+			left: 0;
+			bottom: calc(-1 * var(--shadow-height));
+			background: var(--shadow-gradient);
+		}
+	}
 
-    ul.open :global(li):first-child :global(a) {
-        padding-top: 2.3rem;
-    }
+	@media (min-width: 800px) {
+		.modal-background {
+			display: none;
+		}
 
-    .primary :global(svg) {
-        width: 2rem;
-        height: 2rem;
-    }
+		nav {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+		}
 
-    .home {
-        position: relative;
-        top: -0.1rem;
-        width: 18rem;
-        height: 4.2rem;
-        -webkit-tap-highlight-color: transparent;
-        -webkit-touch-callout: none;
-        background: 0 50% no-repeat;
-        background-size: auto 100%;
-        text-indent: -9999px;
-        /* z-index: 11; */
-    }
+		ul {
+			display: flex;
+			width: auto;
+			height: 100%;
+		}
 
-    ul :global(li).active :global(a) {
-        color: var(--prime);
-    }
+		ul :global(li) {
+			margin: 0 0.5rem;
+			padding: 0;
+		}
 
-    .modal-background {
-        position: fixed;
-        width: 100%;
-        height: 100%;
-        left: 0;
-        top: 0;
-        background-color: rgba(255, 255, 255, 0.9);
-    }
+		ul :global(a) {
+			display: flex;
+			align-items: center;
+			height: 100%;
+		}
 
-    a {
-        color: inherit;
-        border-bottom: none;
-        transition: none;
-    }
+		ul.external {
+			width: 30rem;
+			padding: 0 var(--sk-page-padding-side) 0 0;
+			justify-content: end;
+		}
 
-    ul :global(li):not(.active) :global(a):hover {
-        color: var(--flash);
-    }
-
-    @media (min-width: 840px) {
-        ul {
-            padding: 0;
-            background: none;
-        }
-
-        ul.open {
-            padding: 0;
-            background: white;
-            border: none;
-            align-self: initial;
-        }
-
-        ul.open :global(li) {
-            display: inline;
-            text-align: left;
-        }
-
-        ul.open :global(li) :global(a) {
-            font-size: var(--h5);
-            padding: 0 0.8rem;
-            display: inline;
-        }
-
-        ul::after {
-            display: none;
-        }
-
-        ul :global(li) {
-            display: inline !important;
-        }
-
-        .hide-if-desktop {
-            display: none !important;
-        }
-    }
+		button {
+			display: none;
+		}
+	}
 </style>

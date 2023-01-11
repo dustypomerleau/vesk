17347d7b0bdd1e8bc5d62b4e6b4e036c4289140f
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-01-11 21:04:45 +1100

    new images; optimize image sizes

--- src/lib/doctors.ts
+++ src/lib/doctors.ts
@@ -8,9 +8,9 @@ export const doctors = [
 
             "Dr Tan conducts clinical research in age-related macular degeneration, including a co-investigator role in clinical trials at the Royal Melbourne Hospital. Additional personal research interests include genetic eye disease, diabetic retinopathy, and vitreoretinal surgery."
         ],
-        srcAvif: "mht-50.avif",
-        srcJpg: "mht-50.jpg",
-        srcWebp: "mht-50.webp",
+        srcAvif: "mht.avif",
+        srcJpg: "mht.jpg",
+        srcWebp: "mht.webp",
         alt: "Doctor Mei Tan headshot"
     },
     {
@@ -21,9 +21,9 @@ export const doctors = [
             "Dr Pomerleau received his specialist training at California Pacific Medical Center in San Francisco, followed by a fellowship in medical retina at Duke University, and a vitreoretinal fellowship at the University of Alabama.",
             "Dr Pomerleau's appointment at the Royal Melbourne Hospital includes primary and co-investigator roles in multiple clinical trials for age-related macular degeneration. His personal research interests include macular hole and retinal detachment surgery, as well as inflammatory eye disease."
         ],
-        srcAvif: "dlp-50.avif",
-        srcJpg: "dlp-50.jpg",
-        srcWebp: "dlp-50.webp",
+        srcAvif: "dlp.avif",
+        srcJpg: "dlp.jpg",
+        srcWebp: "dlp.webp",
         alt: "Doctor Dusty Pomerleau headshot"
     }
 ];

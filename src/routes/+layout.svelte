9169fca63e2bce56a56599ac179b175c259b3b01
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-01-16 17:18:05 +0800

    custom leading

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,7 +23,7 @@
 </svelte:head>
 
 <body
-    class="bg-logoNavy font-sans text-lg font-midlight leading-normal text-gray-600"
+    class="bg-logoNavy font-sans text-lg font-midlight leading-[1.55] text-gray-600"
 >
     <Nav />
 

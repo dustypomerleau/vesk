a38c00c8e1f9cf63063e69d131a71c5ff13f2065
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-01-17 12:58:18 +0800

    add mdsvex so svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,20 @@
 import adapter from "@sveltejs/adapter-netlify";
 import preprocess from "svelte-preprocess";
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+    extensions: [".svelte", ".md"],
     // Consult https://github.com/sveltejs/svelte-preprocess
     // for more information about preprocessors
-    preprocess: [preprocess({ postcss: true })],
+    preprocess: [
+        mdsvex({ extensions: [".md"] }),
+        preprocess({ postcss: true })
+    ],
     kit: {
         adapter: adapter()
-        // // added prerender settings per this github issue re: netlify forms:
-        // // https://github.com/sveltejs/kit/issues/942
-        // prerender: {
-        //     crawl: true,
-        //     enabled: true,
-        //     onError: "continue",
-        //     entries: ["*"]
-        // }
+        // previously added (then removed) prerender settings per this github issue re: netlify forms:
+        // https://github.com/sveltejs/kit/issues/942
     }
 };
 

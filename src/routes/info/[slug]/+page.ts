91f85f7e56756932d14eca878c01f31be4cfb91f
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-03-06 14:44:19 +1100

    add date

--- src/routes/info/[slug]/+page.ts
+++ src/routes/info/[slug]/+page.ts
@@ -1,7 +1,7 @@
 export async function load({ params }) {
     const post = await import(`../${params.slug}.md`);
-    const { author, creds, title } = post.metadata;
+    const { author, creds, title, date } = post.metadata;
     const Content = post.default;
 
-    return { author, creds, title, Content };
+    return { author, creds, title, date, Content };
 }

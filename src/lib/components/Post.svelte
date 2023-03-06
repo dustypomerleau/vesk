b0e0a950c8f01d972b1ccac21dc7ddc67325e018
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-03-06 17:09:14 +1100

    inital component for a post in a list

---
+++ src/lib/components/Post.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    export let post;
+    const {
+        path,
+        meta: { author, image, creds, title }
+    } = post;
+</script>
+
+<div class="flex h-auto w-full flex-col bg-blue-300 sm:max-w-xl">
+    <!-- probably remove the image from the posts list and just use it on the actual post page -->
+    <div class="w-1/3 overflow-hidden rounded-t-md">
+        <picture>
+            <source type="image/avif" srcset="{image}.avif" />
+            <source type="image/webp" srcset="{image}.webp" />
+            <source type="image/jpeg" srcset="{image}.jpg" />
+            <img
+                src="{image}.jpg"
+                alt={author}
+                class="object-cover object-top"
+            />
+        </picture>
+    </div>
+    <div>
+        {author}
+        <a href={path}>{title}</a>
+        {creds}
+    </div>
+</div>

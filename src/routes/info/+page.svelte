c2ac1909136777066a40915c61e856f562b25ac0
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-03-06 15:18:16 +1100

    fetch posts before rendering the post list at /info

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,1 +1,18 @@
-list of articles
+<script>
+    export let data;
+    // const {
+    //     posts: {
+    //         meta: { author, creds, title, date },
+    //         Content
+    //     }
+    // } = data;
+</script>
+
+info posts
+
+<ul>
+    <!-- todo: account for situation where there are no posts -->
+    {#each data.posts as post}
+        {post.meta.author}
+    {/each}
+</ul>
---
+++ src/routes/info/+page.ts
@@ -0,0 +1,5 @@
+export const load = async ({ fetch }) => {
+    const response = await fetch("/api/info");
+    const posts = await response.json();
+    return { posts };
+};

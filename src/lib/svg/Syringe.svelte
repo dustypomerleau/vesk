2c4cc8ca8097617d429aafc3e23e762b2f6237f4
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-03-21 16:43:00 +1100

    add SVG icons for IOL, retina, injection

---
+++ src/lib/svg/Syringe.svelte
@@ -0,0 +1,1 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9 6.75 6.75 9l8.125 8.125a1.591 1.591 0 0 0 2.25-2.25L9 6.75ZM7.5 7.5 5.75 5.75m0 0 1-1m-1 1-1 1M17.5 17.5l1.75 1.75"/></svg>

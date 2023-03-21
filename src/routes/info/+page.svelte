843d9b59aee86ef1784807b8b4cf38aa5c5ab9c4
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-03-21 16:41:41 +1100

    margin adjustments

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -4,8 +4,10 @@
 </script>
 
 <!-- todo: what is the best margin for this page on mobile and desktop -->
+<!-- revisit margins more broadly - at what level should you enforce them? -->
+<!-- consider just centering the patient info title? -->
 <div>
-    <div class="mx-10 mt-2 text-2xl font-semibold">Patient Information</div>
+    <div class="ml-9 mr-4 mt-2 text-2xl font-semibold">Patient Information</div>
 
     <ul>
         {#each data.posts as post}

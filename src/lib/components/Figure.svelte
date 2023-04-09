26faead8e84f0c02e9c4d66bac83d1bc7475ae75
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-04-09 18:41:39 +1000

    figure template

---
+++ src/lib/components/Figure.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let image;
+    export let caption;
+</script>
+
+<picture class="rounded-2xl">
+    <source type="image/avif" srcset="{image}.avif" />
+    <source type="image/webp" srcset="{image}.webp" />
+    <source type="image/jpeg" srcset="{image}.jpg" />
+    <img src="{image}.jpg" alt={caption} class="w-full object-cover" />
+</picture>

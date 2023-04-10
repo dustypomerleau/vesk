b6f8501226273bb29a7b771ed064c87af81c1826
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-04-10 21:35:25 +1000

    add CPD registration banner

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,9 +1,14 @@
+<script lang="ts">
+    import Cpd from "$lib/components/Cpd.svelte";
+</script>
+
 <div
     id="hero"
-    class="flex h-screen flex-row items-start justify-center bg-blue-900 bg-hero bg-cover bg-top bg-blend-multiply"
+    class="flex h-screen flex-col items-center justify-start bg-blue-900 bg-hero bg-cover bg-top bg-blend-multiply"
 >
+    <Cpd />
     <h1
-        class="mt-32 w-screen px-7 text-center text-6xl font-light text-white md:mt-60 md:px-32 lg:w-2/3"
+        class="mt-16 w-screen px-7 text-center text-6xl font-light text-white md:mt-60 md:px-32 lg:w-2/3"
     >
         Specialist eye surgeons that care
     </h1>
---
+++ src/lib/components/Cpd.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import Share from "$lib/svg/Share.svelte";
+</script>
+
+<div id="cpd" class="w-full bg-orange-600 px-4 py-2 text-white sm:text-center">
+    Referring doctors can
+    <a
+        href="https://www.eventbrite.com.au/e/vic-eye-retina-specialists-cpd-evening-for-optometrists-tickets-609027857717"
+        target="_blank"
+        rel="noreferrer"
+        class="text-blue-100 shadow-banner hover:shadow-activeBanner"
+        >register here</a
+    >
+    <span class="inline-flex align-middle text-blue-100"><Share /></span>
+    for our upcoming CPD evening.
+</div>

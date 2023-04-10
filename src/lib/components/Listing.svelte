547b9b76e36cf602a70128ca8c27540775b2de3a
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-04-10 15:23:24 +1000

    type the Listing props

--- src/lib/components/Listing.svelte
+++ src/lib/components/Listing.svelte
@@ -1,31 +1,30 @@
 <script lang="ts">
-    export let post: Post;
-    const {
-        path,
-        meta: { author, creds, icon, title }
-    } = post;
-
     import Amd from "$lib/svg/Amd.svelte";
-    import Eye from "$lib/svg/Eye.svelte";
     import Iol from "$lib/svg/Iol.svelte";
-    import Retina from "$lib/svg/Retina.svelte";
     import Syringe from "$lib/svg/Syringe.svelte";
     import Vr from "$lib/svg/Vr.svelte";
 
-    const icons = { Amd, Eye, Iol, Syringe, Retina, Vr };
+    import type { ComponentType, SvelteComponentTyped } from "svelte";
+
+    const icons: Record<string, ComponentType<SvelteComponentTyped>> = {
+        Amd,
+        Iol,
+        Syringe,
+        Vr
+    };
+
+    export let post: Post;
+    const {
+        path,
+        meta: { icon, title }
+    } = post;
 </script>
 
-<!-- also decide on hover effect -->
-<a href={path} class="flex p-4 hover:bg-gray-100">
+<a href={path} class="flex rounded-md p-4 hover:bg-gray-50">
     <div class="flex flex-col content-center justify-center text-blue-700">
         <svelte:component this={icons[icon]} />
     </div>
     <div class="ml-2 flex w-full flex-col">
         <h3 class="font-semibold">{title}</h3>
-        <!-- <div class="my-2 w-5/6 border-b-2 border-gray-300" /> -->
-        <!-- <div class="flex flex-wrap items-baseline">
-            <div class="mr-2">{author}</div>
-            <div class="text-sm text-gray-500">{creds}</div>
-        </div> -->
     </div>
 </a>

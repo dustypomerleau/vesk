44d07e6d82bf8e5acded4410869ad1dcf1f78a88
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-04-10 14:58:55 +1000

    reduce rounding of image

--- src/lib/components/Figure.svelte
+++ src/lib/components/Figure.svelte
@@ -3,7 +3,7 @@
     export let caption: string;
 </script>
 
-<picture class="rounded-2xl">
+<picture class="rounded-md">
     <source type="image/avif" srcset="{image}.avif" />
     <source type="image/webp" srcset="{image}.webp" />
     <source type="image/jpeg" srcset="{image}.jpg" />

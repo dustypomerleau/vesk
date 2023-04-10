c77c9904af9b81b1067991f37eaf4f43505c0cee
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-04-10 17:27:46 +1000

    type the glob import function and add notes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,13 @@
-export const getPosts: Promise<Post[]> = async () => {
-    const postFiles = import.meta.glob("/src/routes/info/*.md");
+export const getPosts = async (): Promise<Array<Post>> => {
+    const postFiles = import.meta.glob<Record<string, PostMetadata>>(
+        "/src/routes/info/*.md"
+    );
     const iterableFiles = Object.entries(postFiles);
 
-    const posts = await Promise.all(
+    // Object.entries returns an array of arrays, where each array is a KV pair from the object
+    // That means postFiles returns an object with keys of path and values of resolver functions that take no params and return the result of importing the file.
+    // In this case, calling the resolver() (essentially calling import on the md file) appears to return the files metadata as metadata (I think it also returns the content as default, but we aren't binding it here)
+    const posts: Array<Post> = await Promise.all(
         iterableFiles.map(async ([path, resolver]) => {
             const { metadata } = await resolver();
             // trim /src/routes and .md to get the URL

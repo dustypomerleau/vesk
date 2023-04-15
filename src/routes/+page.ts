0f3f965d9268c779fc598989d9d322baa082f587
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-04-15 17:11:21 +1000

    document the type of the fetch param

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,5 @@
+// type of fetch param is (input: Requestinfo | URL, init?: RequestInit | undefined): Promise<Response>
+// The brackets around fetch here are just destructuring.
 export const load = async ({ fetch }): Promise<{ posts: Array<Post> }> => {
     const response = await fetch("/api/info");
     const posts = await response.json();

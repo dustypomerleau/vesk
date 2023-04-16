6316d673e635a9352d4c8552238c1c0a5d388aca
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-04-16 15:21:41 +1000

    update notes

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,6 @@
 import type { PostPath } from "$lib/types";
 
-// type of fetch param is (input: RequestInfo | URL, init?: RequestInit | undefined): Promise<Response>
+// typeof fetch is (input: RequestInfo | URL, init?: RequestInit | undefined): Promise<Response>
 // The brackets around fetch here are just destructuring.
 export const load = async ({ fetch }): Promise<{ posts: Array<PostPath> }> => {
     const response = await fetch("/api/info");

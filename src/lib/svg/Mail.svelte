d720f8000d27d869b86c6aa286f0a46a6f481096
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-04-17 19:58:09 +1000

    viewBox prop with default adjustments for LinkButton icons

--- src/lib/svg/Mail.svelte
+++ src/lib/svg/Mail.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     export let width: string;
+    export let viewBox = "0 0.5 24 24";
 </script>
 
-<svg viewBox="0 0 24 24" {width} xmlns="http://www.w3.org/2000/svg" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.75 7.75a2 2 0 0 1 2-2h10.5a2 2 0 0 1 2 2v8.5a2 2 0 0 1-2 2H6.75a2 2 0 0 1-2-2v-8.5z"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m5.5 6.5 6.5 5.75 6.5-5.75"/></svg>
+<svg {viewBox} {width} xmlns="http://www.w3.org/2000/svg" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.75 7.75a2 2 0 0 1 2-2h10.5a2 2 0 0 1 2 2v8.5a2 2 0 0 1-2 2H6.75a2 2 0 0 1-2-2v-8.5z"/><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m5.5 6.5 6.5 5.75 6.5-5.75"/></svg>

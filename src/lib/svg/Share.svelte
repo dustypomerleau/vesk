2f5d9cb6cc6e00bee2d2ddf43d536194e43f641e
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-04-17 20:01:55 +1000

    viewBox prop with default adjustments for Share icon

--- src/lib/svg/Share.svelte
+++ src/lib/svg/Share.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     export let width: string;
+    export let viewBox = "0 0.8 24 24"
 </script>
 
-<svg viewBox="0 0 24 24" {width} xmlns="http://www.w3.org/2000/svg" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9.25 4.75h-2.5a2 2 0 0 0-2 2v10.5a2 2 0 0 0 2 2h10.5a2 2 0 0 0 2-2v-2.5M19.25 9.25v-4.5h-4.5M19 5l-7.25 7.25"/></svg>
+<svg {viewBox} {width} xmlns="http://www.w3.org/2000/svg" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9.25 4.75h-2.5a2 2 0 0 0-2 2v10.5a2 2 0 0 0 2 2h10.5a2 2 0 0 0 2-2v-2.5M19.25 9.25v-4.5h-4.5M19 5l-7.25 7.25"/></svg>

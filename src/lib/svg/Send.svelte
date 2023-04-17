106f56137ef72463122bfdf9520d9c791833c3d6
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-04-17 20:04:46 +1000

    viewBox prop with default adjustments for Send icon

--- src/lib/svg/Send.svelte
+++ src/lib/svg/Send.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     export let width: string;
+    export let viewBox = "0 0.6 24 24"
 </script>
 
-<svg viewBox="0 0 24 24" {width} xmlns="http://www.w3.org/2000/svg" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.75 19.25 12 4.75l7.25 14.5-7.25-3.5-7.25 3.5zM12 15.5v-2.75"/></svg>
+<svg {viewBox} {width} xmlns="http://www.w3.org/2000/svg" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.75 19.25 12 4.75l7.25 14.5-7.25-3.5-7.25 3.5zM12 15.5v-2.75"/></svg>

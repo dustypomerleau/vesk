c39182482960e54492028742414b7558e094bf9b
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-04-21 10:09:42 +1000

    remove empty width class

--- src/lib/components/SvgFigure.svelte
+++ src/lib/components/SvgFigure.svelte
@@ -17,9 +17,7 @@
         <svelte:component this={svg} {viewBox} width="100%" />
     </div>
     {#if caption}
-        <div
-            class="w-full {smallBreakpointWidth} w- px-2 text-base text-gray-500"
-        >
+        <div class="w-full {smallBreakpointWidth} px-2 text-base text-gray-500">
             {caption}
         </div>
     {/if}

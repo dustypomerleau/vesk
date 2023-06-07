cf319b80e95ce39aa0bbe70064cd71aa15c6ee82
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-06-07 12:07:42 +1000

    make the end date of copyright dynamic

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    const currYear = new Date().getFullYear();
+
     const links = [
         { href: "/", text: "Home" },
         { href: "/#doctors", text: "Specialists" },
@@ -13,7 +15,7 @@
     class="mt-auto flex flex-col gap-3 bg-gray-4 px-6 py-4 sm:items-center sm:justify-center sm:px-12 lg:flex-row lg:gap-12"
 >
     <div class="font-normal text-gray-8">
-        &copy;2022&ndash;2023 Vic&nbsp;Eye&nbsp;and&nbsp;Retina&nbsp;Specialists Pty&nbsp;Ltd
+        &copy;2022&ndash;{currYear} Vic&nbsp;Eye&nbsp;and&nbsp;Retina&nbsp;Specialists Pty&nbsp;Ltd
     </div>
     <div
         class="sm: grid grid-cols-2 items-center gap-3 sm:flex sm:grid-cols-none sm:flex-row sm:justify-center sm:gap-6"

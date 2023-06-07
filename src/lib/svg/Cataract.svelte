616904b3946a67626a6fc6008a475bd850875858
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-06-07 16:48:19 +1000

    add scatter and blur to cataract figure

--- src/lib/svg/Cataract.svelte
+++ src/lib/svg/Cataract.svelte
@@ -4,21 +4,40 @@
 </script>
 
 <svg {viewBox} {width} fill="none">
-    <rect width="1200" height="800" fill="#183863"/>
-    <path d="M770.639 399.946L1015.46 327V472.5L770.639 399.946L703.335 420C703.335 420 702.267 408.005 702.272 399.906C702.277 391.881 703.335 380 703.335 380L770.639 399.946Z" fill="url(#paint0_radial_1_60)" fill-opacity="0.3"/>
-    <path d="M699.129 400C699.129 508.991 782.482 597.345 885.303 597.345C954.087 597.345 1015.54 556.428 1047.78 497.621C1073.8 479.194 1090.29 444.08 1090.29 402.077C1090.29 361.695 1075.07 327.831 1050.68 308.793C1019.42 246.361 956.631 202.655 885.303 202.655C782.482 202.655 699.129 291.009 699.129 400Z" stroke="#6A7789" stroke-opacity="0.5" stroke-width="6"/>
-    <ellipse cx="1015.43" cy="400" rx="32.5314" ry="73.1217" fill="#9D770F" fill-opacity="0.7"/>
-    <path d="M120.25 400L431.625 327.125V472.625L120.25 400Z" fill="url(#paint1_linear_1_60)" fill-opacity="0.8"/>
-    <path d="M115.419 400C115.419 508.991 198.772 597.345 301.593 597.345C370.377 597.345 431.833 556.428 464.067 497.621C490.086 479.194 506.581 444.08 506.581 402.077C506.581 361.695 491.355 327.831 466.97 308.793C435.706 246.361 372.921 202.655 301.593 202.655C198.772 202.655 115.419 291.009 115.419 400Z" stroke="#6A7789" stroke-opacity="0.5" stroke-width="6"/>
-    <ellipse cx="431.719" cy="400" rx="32.5314" ry="73.1217" fill="#86B8E9" fill-opacity="0.8"/>
-    <defs>
-    <radialGradient id="paint0_radial_1_60" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(770.46 399.75) rotate(0.350785) scale(245.005 527.37)">
-    <stop stop-color="#9D770F"/>
-    <stop offset="1" stop-color="#D9D9D9" stop-opacity="0"/>
-    </radialGradient>
-    <linearGradient id="paint1_linear_1_60" x1="120" y1="399.875" x2="431.625" y2="399.875" gradientUnits="userSpaceOnUse">
-    <stop stop-color="white"/>
-    <stop offset="1" stop-color="white" stop-opacity="0"/>
-    </linearGradient>
-    </defs>
-    </svg>
+<rect width="1200" height="800" fill="#183863"/>
+<g filter="url(#filter0_f_1_60)">
+<path d="M1017.5 472.5V327.5L756 263C756 263 703 313.5 703 400C703 486.5 756 537 756 537L1017.5 472.5Z" fill="url(#paint0_diamond_1_60)" fill-opacity="0.2"/>
+</g>
+<g filter="url(#filter1_f_1_60)">
+<path d="M770.64 399.946L1015.46 327V472.5L770.64 399.946L703.335 420C703.335 420 702.267 408.005 702.272 399.906C702.277 391.881 703.335 380 703.335 380L770.64 399.946Z" fill="url(#paint1_radial_1_60)" fill-opacity="0.4"/>
+</g>
+<path d="M699.129 400C699.129 508.991 782.482 597.345 885.303 597.345C954.087 597.345 1015.54 556.428 1047.78 497.621C1073.8 479.194 1090.29 444.08 1090.29 402.077C1090.29 361.695 1075.07 327.831 1050.68 308.793C1019.42 246.361 956.631 202.655 885.303 202.655C782.482 202.655 699.129 291.009 699.129 400Z" stroke="#6A7789" stroke-opacity="0.5" stroke-width="6"/>
+<ellipse cx="1015.43" cy="400" rx="32.5314" ry="73.1217" fill="#9D770F" fill-opacity="0.7"/>
+<path d="M120.25 400L431.625 327.125V472.625L120.25 400Z" fill="url(#paint2_linear_1_60)" fill-opacity="0.8"/>
+<path d="M115.419 400C115.419 508.991 198.772 597.345 301.593 597.345C370.377 597.345 431.833 556.428 464.067 497.621C490.086 479.194 506.581 444.08 506.581 402.077C506.581 361.695 491.355 327.831 466.97 308.793C435.706 246.361 372.921 202.655 301.593 202.655C198.772 202.655 115.419 291.009 115.419 400Z" stroke="#6A7789" stroke-opacity="0.5" stroke-width="6"/>
+<ellipse cx="431.719" cy="400" rx="32.5314" ry="73.1217" fill="#86B8E9" fill-opacity="0.8"/>
+<defs>
+<filter id="filter0_f_1_60" x="693" y="253" width="334.5" height="294" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+<feGaussianBlur stdDeviation="5" result="effect1_foregroundBlur_1_60"/>
+</filter>
+<filter id="filter1_f_1_60" x="698.272" y="323" width="321.188" height="153.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+<feGaussianBlur stdDeviation="2" result="effect1_foregroundBlur_1_60"/>
+</filter>
+<radialGradient id="paint0_diamond_1_60" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(703 400.75) scale(314.5 362.973)">
+<stop stop-color="#D9D9D9" stop-opacity="0"/>
+<stop offset="1" stop-color="#9D770F"/>
+</radialGradient>
+<radialGradient id="paint1_radial_1_60" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(770.46 399.75) rotate(0.350785) scale(245.005 527.37)">
+<stop stop-color="#9D770F"/>
+<stop offset="1" stop-color="#D9D9D9" stop-opacity="0"/>
+</radialGradient>
+<linearGradient id="paint2_linear_1_60" x1="120" y1="399.875" x2="431.625" y2="399.875" gradientUnits="userSpaceOnUse">
+<stop stop-color="white"/>
+<stop offset="1" stop-color="white" stop-opacity="0"/>
+</linearGradient>
+</defs>
+</svg>

9940eab8e1accba093c77e3a1e1c361d61f606fd
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-06-08 17:14:49 +1000

    add IOL comparisons at distance and near

---
+++ src/lib/svg/IolDistance.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    export let viewBox = "0 0 1200 800";
+    export let width = "100%";
+</script>
+
+<svg {viewBox} {width} fill="none">
+<rect width="1200" height="800" fill="#183863"/>
+<path d="M1050 400C1050 508.991 966.647 597.345 863.826 597.345C795.042 597.345 733.586 556.428 701.353 497.621C675.334 479.194 658.839 444.08 658.839 402.077C658.839 361.695 674.064 327.831 698.45 308.793C729.713 246.361 792.498 202.655 863.826 202.655C966.647 202.655 1050 291.009 1050 400Z" stroke="#6A7789" stroke-opacity="0.5" stroke-width="6"/>
+<path d="M738.685 326.878L976.689 400.346L1045.24 379.674C1045.24 379.674 1046.21 392.362 1046.21 400.512C1046.21 408.723 1045.24 421.505 1045.24 421.505L976.689 400.346L738.685 472.122V326.878Z" fill="url(#paint0_radial_10_2)" fill-opacity="0.8"/>
+<path d="M1046.21 400L740.381 340.375V459.625L1046.21 400Z" fill="url(#paint1_linear_10_2)"/>
+<rect x="731.81" y="326.878" width="13.75" height="146.243" rx="5" fill="#90CBFF" fill-opacity="0.5"/>
+<path d="M495.169 400L183.794 327.25V472.75L495.169 400Z" fill="url(#paint2_linear_10_2)"/>
+<path d="M500 400C500 508.991 416.647 597.345 313.826 597.345C245.042 597.345 183.586 556.428 151.353 497.621C125.334 479.194 108.839 444.08 108.839 402.077C108.839 361.695 124.064 327.831 148.45 308.793C179.713 246.361 242.498 202.655 313.826 202.655C416.647 202.655 500 291.009 500 400Z" stroke="#6A7789" stroke-opacity="0.5" stroke-width="6"/>
+<rect x="181.808" y="326.878" width="13.75" height="146.243" rx="5" fill="#90CBFF" fill-opacity="0.5"/>
+<defs>
+<radialGradient id="paint0_radial_10_2" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(975.726 399.5) rotate(179.136) scale(237.068 656.684)">
+<stop offset="0.0235546" stop-color="#FF7B00"/>
+<stop offset="1" stop-color="#FF7B00" stop-opacity="0"/>
+</radialGradient>
+<linearGradient id="paint1_linear_10_2" x1="1046.21" y1="400" x2="754.692" y2="400.331" gradientUnits="userSpaceOnUse">
+<stop stop-color="#ACCDED"/>
+<stop offset="1" stop-color="#ACCDED" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint2_linear_10_2" x1="495.169" y1="400" x2="183.794" y2="400" gradientUnits="userSpaceOnUse">
+<stop offset="0.00389532" stop-color="#ACCDED"/>
+<stop offset="1" stop-color="#ACCDED" stop-opacity="0"/>
+</linearGradient>
+</defs>
+</svg>

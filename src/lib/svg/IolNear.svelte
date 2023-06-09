3ba6ef8ee23c58f3ccfaafd27b4630bcb8c599a4
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-06-09 17:34:01 +1000

    adjust ray length on multifocal near and improve dashes

--- src/lib/svg/IolNear.svelte
+++ src/lib/svg/IolNear.svelte
@@ -7,21 +7,21 @@
 <rect width="1200" height="800" fill="#183863"/>
 <path d="M1050 400C1050 508.991 966.647 597.345 863.826 597.345C795.042 597.345 733.586 556.428 701.353 497.621C675.334 479.194 658.839 444.08 658.839 402.077C658.839 361.695 674.064 327.83 698.45 308.793C729.713 246.361 792.498 202.655 863.826 202.655C966.647 202.655 1050 291.009 1050 400Z" stroke="#6A7789" stroke-opacity="0.5" stroke-width="6"/>
 <g opacity="0.8">
-<path d="M1046.78 390.219L1095.08 400.094L1046.78 409.781" stroke="#ACCDED" stroke-width="3" stroke-linejoin="round" stroke-dasharray="2 6"/>
-<path d="M738.685 472.75L1047.35 410.344C1047.35 410.344 1047.56 404.438 1047.56 400.062C1047.56 394.984 1047.35 389.719 1047.35 389.719L738.685 327.25V472.75Z" fill="url(#paint0_linear_10_23)"/>
+<path d="M1046.78 390.219L1095.08 400.094L1046.78 409.781" stroke="#ACCDED" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" stroke-dasharray="2 6"/>
+<path d="M738.685 472.75L1046.56 410.344C1046.56 410.344 1046.78 404.438 1046.78 400.062C1046.78 394.984 1046.56 389.719 1046.56 389.719L738.685 327.25V472.75Z" fill="url(#paint0_linear_10_23)"/>
 </g>
-<path d="M1047.12 400.25L738.685 326.878V473.122L1047.12 400.25Z" fill="url(#paint1_linear_10_23)"/>
+<path d="M1046.78 400.25L738.685 326.878V473.122L1046.78 400.25Z" fill="url(#paint1_linear_10_23)"/>
 <rect x="731.81" y="326.878" width="13.75" height="146.243" rx="5" fill="#90CBFF" fill-opacity="0.5"/>
 <path d="M500 400C500 508.991 416.647 597.345 313.826 597.345C245.042 597.345 183.587 556.428 151.353 497.621C125.334 479.194 108.839 444.08 108.839 402.077C108.839 361.695 124.064 327.831 148.45 308.793C179.713 246.361 242.498 202.655 313.826 202.655C416.647 202.655 500 291.009 500 400Z" stroke="#6A7789" stroke-opacity="0.5" stroke-width="6"/>
-<path d="M496.813 390.25L545.109 400.125L496.813 409.812" stroke="#ACCDED" stroke-width="3" stroke-linejoin="round" stroke-dasharray="2 6"/>
+<path d="M496.813 390.25L545.109 400.125L496.813 409.812" stroke="#ACCDED"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-dasharray="2 6"/>
 <path d="M183.795 472.75L496.719 410.344C496.719 410.344 496.938 404.438 496.938 400.062C496.939 394.984 496.719 389.719 496.719 389.719L183.795 327.25V472.75Z" fill="url(#paint2_linear_10_23)" fill-opacity="0.8"/>
 <rect x="181.808" y="326.878" width="13.75" height="146.243" rx="5" fill="#90CBFF" fill-opacity="0.5"/>
 <defs>
-<linearGradient id="paint0_linear_10_23" x1="1096" y1="400" x2="738.684" y2="400" gradientUnits="userSpaceOnUse">
+<linearGradient id="paint0_linear_10_23" x1="1095.1" y1="400" x2="738.684" y2="400" gradientUnits="userSpaceOnUse">
 <stop offset="0.00389532" stop-color="#ACCDED"/>
 <stop offset="1" stop-color="#ACCDED" stop-opacity="0"/>
 </linearGradient>
-<linearGradient id="paint1_linear_10_23" x1="1046" y1="400" x2="753.065" y2="400.273" gradientUnits="userSpaceOnUse">
+<linearGradient id="paint1_linear_10_23" x1="1045.66" y1="400" x2="753.049" y2="400.272" gradientUnits="userSpaceOnUse">
 <stop stop-color="#FF7B00"/>
 <stop offset="1" stop-color="#FF7B00" stop-opacity="0"/>
 </linearGradient>

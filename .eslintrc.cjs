0cd9f8cd22333dbc72b54333f97385fbf4531d9e
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-09-23 17:28:04 +1000

    migrate to new svelte eslint plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,45 @@
+// see https://eslint.org/docs/latest/use/configure/configuration-files-new
+// to migrate to the new format
+
 module.exports = {
+    extends: [
+        "eslint:recommended",
+        "prettier",
+        "plugin:svelte/prettier",
+        "plugin:@typescript-eslint/strict-type-checked",
+        "plugin:@typescript-eslint/stylistic-type-checked",
+    ],
     root: true,
     parser: "@typescript-eslint/parser",
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-    plugins: ["svelte3", "@typescript-eslint"],
-    ignorePatterns: ["*.cjs"],
-    overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
-    settings: {
-        "svelte3/typescript": () => require("typescript"),
-    },
     parserOptions: {
+        extraFileExtensions: [".svelte"], // required by `@typescript-eslint/parser` v4.24.0.
         sourceType: "module",
         ecmaVersion: 2020,
+        project: true,
+        tsconfigRootDir: __dirname,
     },
+    overrides: [
+        {
+            files: ["*.svelte"],
+            parser: "svelte-eslint-parser",
+            // parse the `<script>` in `.svelte` as typescript:
+            parserOptions: {
+                parser: "@typescript-eslint/parser",
+            },
+        },
+    ],
+    plugins: ["@typescript-eslint"],
     env: {
         browser: true,
         es2017: true,
         node: true,
+    },
+    // ignorePatterns: ["*.cjs"],
+    settings: {
+        compileOptions: {
+            postcss: {
+                configFilePath: "./postcss.config.cjs",
+            },
+        },
     },
 };

3d9d81f2961727f44a3dbc42d7bcc0e567799b76
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-09-24 16:42:55 +1000

    notes

--- svelte.config.js
+++ svelte.config.js
@@ -5,8 +5,7 @@ import { mdsvex } from "mdsvex";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     extensions: [".svelte", ".md"],
-    // the issues you had with sending width to figures may have to do with tailwind running before mdsvex - can these be reversed?
-    preprocess: [preprocess({ postcss: true }), mdsvex({ extensions: [".svelte", ".md"] })],
+    preprocess: [mdsvex({ extensions: [".svelte", ".md"] }), preprocess({ postcss: true }),],
     kit: {
         adapter: adapter(),
     },

19d3600663cf2f9f09178bea0e86ac799c9dac98
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2023-11-25 19:56:11 +1100

    add intravitreal versuse orbital floor injection schematic for patient info article

---
+++ src/lib/svg/Injection.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+    export let viewBox = "0 0 1200 800";
+    export let width = "100%";
+</script>
+
+<svg {viewBox} {width} fill="none">
+<rect width="1200" height="800" fill="#183863"/>
+<path d="M742.16 204.5L919.16 102" stroke="url(#paint0_linear_101_2)" stroke-opacity="0.8" stroke-width="9"/>
+<path d="M907.16 86L926.66 119.5" stroke="url(#paint1_linear_101_2)" stroke-width="7" stroke-linecap="round"/>
+<path d="M695.124 214.51L718.66 200.5L751.66 215L709.493 239.398L695.124 214.51Z" fill="#1EA8EE"/>
+<path d="M474.161 373C406.266 292.017 603.267 274.816 637.661 264C605.161 280.5 557.161 472 474.161 373Z" fill="url(#paint2_linear_101_2)"/>
+<path d="M639.14 261.141L701.739 225L703.679 228.36L638.017 265.728C637.594 265.969 637.112 265.525 637.316 265.083L639.14 261.141Z" fill="url(#paint3_linear_101_2)"/>
+<path d="M696.959 216.58C695.993 214.906 696.566 212.766 698.24 211.799L883.57 104.799C885.244 103.833 887.384 104.406 888.351 106.08L899.851 125.999C900.817 127.673 900.244 129.813 898.57 130.78L713.24 237.78C711.566 238.746 709.426 238.173 708.459 236.499L702.709 226.539L696.959 216.58Z" stroke="url(#paint4_linear_101_2)" stroke-width="3"/>
+<rect x="727.136" y="196.844" width="15.7745" height="26.9944" rx="3" transform="rotate(-30 727.136 196.844)" fill="url(#paint5_linear_101_2)"/>
+<path d="M336 380.345C336 271.354 420.205 183 524.078 183C593.565 183 655.649 223.917 688.213 282.724C714.497 301.151 731.161 336.266 731.161 378.268C731.161 418.65 715.78 452.515 691.145 471.553C659.562 533.984 596.135 577.691 524.078 577.691C420.205 577.691 336 489.336 336 380.345Z" stroke="#6A7789" stroke-opacity="0.5" stroke-width="6"/>
+<ellipse cx="32.8641" cy="73.1217" rx="32.8641" ry="73.1217" transform="matrix(1 0 0 -1 622.67 453.467)" fill="#6A7789" fill-opacity="0.5"/>
+<path d="M726.161 540.5L903.161 438" stroke="url(#paint6_linear_101_2)" stroke-opacity="0.8" stroke-width="9"/>
+<path d="M891.161 422L910.661 455.5" stroke="url(#paint7_linear_101_2)" stroke-width="7" stroke-linecap="round"/>
+<path d="M679.126 550.51L702.661 536.5L735.661 551L693.494 575.398L679.126 550.51Z" fill="#FF7B00" fill-opacity="0.8"/>
+<path d="M443.161 680C417.161 634 446.161 598.5 475.161 591C514.161 580.914 549.161 608.5 621.661 599C586.161 680 474.534 735.506 443.161 680Z" fill="url(#paint8_linear_101_2)"/>
+<path d="M623.141 597.141L685.74 561L687.68 564.36L622.018 601.728C621.595 601.969 621.112 601.525 621.317 601.083L623.141 597.141Z" fill="url(#paint9_linear_101_2)"/>
+<path d="M680.96 552.58C679.994 550.906 680.567 548.766 682.241 547.799L867.571 440.799C869.245 439.833 871.385 440.406 872.352 442.08L883.852 461.999C884.818 463.673 884.245 465.813 882.571 466.78L697.241 573.78C695.567 574.746 693.427 574.173 692.46 572.499L686.71 562.539L680.96 552.58Z" stroke="url(#paint10_linear_101_2)" stroke-width="3"/>
+<rect x="711.136" y="532.844" width="15.7745" height="26.9944" rx="3" transform="rotate(-30 711.136 532.844)" fill="url(#paint11_linear_101_2)"/>
+<defs>
+<linearGradient id="paint0_linear_101_2" x1="806.66" y1="161.5" x2="811.66" y2="170" gradientUnits="userSpaceOnUse">
+<stop stop-color="#B2BCC6"/>
+<stop offset="1" stop-color="#6A7789"/>
+</linearGradient>
+<linearGradient id="paint1_linear_101_2" x1="906.16" y1="83.5" x2="928.66" y2="122" gradientUnits="userSpaceOnUse">
+<stop stop-color="#B2BCC6"/>
+<stop offset="0.47473" stop-color="#8E9BAB"/>
+</linearGradient>
+<linearGradient id="paint2_linear_101_2" x1="638.161" y1="263.499" x2="465.161" y2="371" gradientUnits="userSpaceOnUse">
+<stop stop-color="#1EA8EE"/>
+<stop offset="1" stop-color="#D9D9D9" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint3_linear_101_2" x1="668.35" y1="244.277" x2="670.29" y2="247.638" gradientUnits="userSpaceOnUse">
+<stop stop-color="#D1D6DC"/>
+<stop offset="1" stop-color="#B2BCC6"/>
+</linearGradient>
+<linearGradient id="paint4_linear_101_2" x1="790.155" y1="157" x2="806.655" y2="185.579" gradientUnits="userSpaceOnUse">
+<stop stop-color="#8E9BAB"/>
+<stop offset="1" stop-color="#6A7789"/>
+</linearGradient>
+<linearGradient id="paint5_linear_101_2" x1="735.023" y1="196.844" x2="735.023" y2="223.839" gradientUnits="userSpaceOnUse">
+<stop stop-color="#4B5563"/>
+<stop offset="1" stop-color="#323A45"/>
+</linearGradient>
+<linearGradient id="paint6_linear_101_2" x1="790.661" y1="497.5" x2="795.661" y2="506" gradientUnits="userSpaceOnUse">
+<stop stop-color="#B2BCC6"/>
+<stop offset="1" stop-color="#6A7789"/>
+</linearGradient>
+<linearGradient id="paint7_linear_101_2" x1="890.161" y1="419.5" x2="912.661" y2="458" gradientUnits="userSpaceOnUse">
+<stop stop-color="#B2BCC6"/>
+<stop offset="0.47473" stop-color="#8E9BAB"/>
+</linearGradient>
+<linearGradient id="paint8_linear_101_2" x1="622.161" y1="598.5" x2="433.161" y2="677" gradientUnits="userSpaceOnUse">
+<stop stop-color="#FF7B00"/>
+<stop offset="1" stop-color="#D9D9D9" stop-opacity="0"/>
+</linearGradient>
+<linearGradient id="paint9_linear_101_2" x1="652.35" y1="580.277" x2="654.29" y2="583.638" gradientUnits="userSpaceOnUse">
+<stop stop-color="#D1D6DC"/>
+<stop offset="1" stop-color="#B2BCC6"/>
+</linearGradient>
+<linearGradient id="paint10_linear_101_2" x1="774.156" y1="493" x2="790.656" y2="521.579" gradientUnits="userSpaceOnUse">
+<stop stop-color="#8E9BAB"/>
+<stop offset="1" stop-color="#6A7789"/>
+</linearGradient>
+<linearGradient id="paint11_linear_101_2" x1="719.024" y1="532.844" x2="719.024" y2="559.839" gradientUnits="userSpaceOnUse">
+<stop stop-color="#4B5563"/>
+<stop offset="1" stop-color="#323A45"/>
+</linearGradient>
+</defs>
+</svg>
+

ba20af0faeaa0bcfe4180eb265f6646ca0e7f40b
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-02-27 15:46:45 +1100

    feat(auth): add initial clerk paths and components

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
     import Logo from "$lib/svg/Logo.svelte";
+    import UserButton from "clerk-sveltekit/client/UserButton.svelte";
+    import SignedIn from "clerk-sveltekit/client/SignedIn.svelte";
+    import SignedOut from "clerk-sveltekit/client/SignedOut.svelte";
 </script>
 
 <header id="header" class="flex flex-col items-center justify-between bg-blue-10 md:flex-row">
@@ -8,4 +11,14 @@
             <Logo />
         </div>
     </a>
+    <div class="px-8 py-2 md:py-4">
+        <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+        <SignedOut>
+            <a href="/login" class="text-gray-4 underline hover:text-gray-3 hover:decoration-2"
+                >Provider login</a
+            >
+        </SignedOut>
+    </div>
 </header>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import SignIn from "clerk-sveltekit/client/SignIn.svelte";
+</script>
+
+<div class="flex justify-center bg-blue-10 pb-8 pt-4 md:pt-10">
+    <SignIn
+        redirectUrl="/provider"
+        appearance={{
+            variables: {
+                colorText: "#4b5563",
+            },
+        }}
+    />
+</div>

471f510fb1a22aa0f4b32f5d38a881bd0cdd20e9
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-03-11 10:16:50 +1100

    fix(auth): use `ClerkLoaded` to get access to the `User` on `window`

--- src/routes/provider/+layout.svelte
+++ src/routes/provider/+layout.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
+    import ClerkLoading from "clerk-sveltekit/client/ClerkLoading.svelte";
     import type { LayoutData } from "./$types";
+    import ClerkLoaded from "clerk-sveltekit/client/ClerkLoaded.svelte";
 
-    export let data: LayoutData;
-    console.log(data);
+    // export let data: LayoutData;
+    // console.log(data);
 </script>
 
 provider layout file - this content is always visible
-{data.isProvider}
-{data.isProviderRole}
 
-{#if data.isProvider}
+<ClerkLoading let:clerk>clerk is loading</ClerkLoading>
+<ClerkLoaded let:clerk
+    >clerk is fully loaded
+    {window.Clerk.user?.fullName}
     <slot />
-{/if}
+</ClerkLoaded>

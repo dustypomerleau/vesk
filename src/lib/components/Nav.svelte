2de6b390095759a37795495a44cf34f5dff8b71b
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-03-12 23:50:26 +1100

    style(clerk): basic styling of clerk components

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -6,24 +6,29 @@
     import UserButton from "clerk-sveltekit/client/UserButton.svelte";
 </script>
 
-<header id="header" class="flex flex-row items-center justify-between bg-blue-10 px-4 py-4 sm:px-8">
+<header
+    id="header"
+    class="flex flex-row flex-wrap items-center justify-between bg-blue-10 px-4 py-4 sm:px-8"
+>
     <a href="/" title="Vic Eye Home">
         <div>
             <Logo />
         </div>
     </a>
-    <div>
-        <SignedIn>
-            <UserButton afterSignOutUrl="/" />
+
+    <SignedIn>
+        <div class="flex flex-wrap items-center gap-4 pl-1 pt-4">
             <!-- todo: how best to get the user into the correct organization, can we default to viceye rather than personal? Can we make viceye the only option? -->
-            <OrganizationSwitcher />
-        </SignedIn>
-        <SignedOut>
-            <div class="flex flex-col justify-center pl-2 pt-3 sm:pt-0">
-                <a href="/login" class="text-gray-4 underline hover:text-gray-3 hover:decoration-2"
-                    >Provider login</a
-                >
-            </div>
-        </SignedOut>
-    </div>
+            <div><OrganizationSwitcher /></div>
+            <div><UserButton afterSignOutUrl="/" /></div>
+        </div>
+    </SignedIn>
+
+    <SignedOut>
+        <div class="flex flex-col justify-center pl-2 pt-3 sm:pt-0">
+            <a href="/login" class="text-gray-4 underline hover:text-gray-3 hover:decoration-2"
+                >Provider login</a
+            >
+        </div>
+    </SignedOut>
 </header>

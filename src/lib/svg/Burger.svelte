f23fbe11459b777bf201f88687fd632a2fe8f248
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-03-15 09:50:57 +1100

    feat(icons): add `name` prop to `Burger`

--- src/lib/svg/Burger.svelte
+++ src/lib/svg/Burger.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+    export let name = "menu"
     export let viewBox = "0 0.6 24 24";
     export let width = "1.5rem";
 </script>
 
-<svg {viewBox} {width} xmlns="http://www.w3.org/2000/svg" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.75 5.75h14.5m-14.5 12.5h14.5M4.75 12h14.5"/></svg>
+<svg {name} {viewBox} {width} xmlns="http://www.w3.org/2000/svg" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.75 5.75h14.5m-14.5 12.5h14.5M4.75 12h14.5"/></svg>

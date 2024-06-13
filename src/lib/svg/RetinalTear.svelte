e5b72f2aaf5763aa1efc01a295adddd576401e00
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-06-13 17:32:13 +1000

    fix(SVG): "unoptimize" the retinal tear SVG and use runes syntax on new SVGs

--- src/lib/svg/RetinalTear.svelte
+++ src/lib/svg/RetinalTear.svelte
@@ -1,7 +1,6 @@
 <!-- rotated version of dotted-circle-right -->
 <script lang="ts">
-    export let viewBox = "0 0 24 24";
-    export let width = "1.5rem";
+    let { viewBox = "0 0 24 24", width = "1.5rem" } = $props();
 </script>
 
 <svg transform="rotate(-70)" {viewBox} {width} xmlns="http://www.w3.org/2000/svg" fill="none"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.75" d="M6.742 17.415A7.2 7.2 0 0 0 9.25 19.25M5.025 9.834A8.6 8.6 0 0 0 4.75 12c0 .751.095 1.478.275 2.166M9.25 4.75a7.2 7.2 0 0 0-2.508 1.835M11.75 17.25c0 1.105.911 2.025 1.976 1.733 3.112-.854 5.524-3.657 5.524-6.983s-2.412-6.129-5.524-6.983c-1.065-.292-1.976.628-1.976 1.733z"/></svg>

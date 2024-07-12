958bae33672edd577c5d2f3c6dbbef87d5817575
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-07-12 16:30:49 +1000

    build(lint): eslint config changes

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,21 @@
 // examples of the new config:
 // https://github.com/sveltejs/eslint-plugin-svelte/issues/732
 
+// @ts-check
+
 import eslint from "@eslint/js";
 import prettier from "eslint-config-prettier";
 import svelte from "eslint-plugin-svelte";
 import svelteParser from "svelte-eslint-parser";
-import typescript from "typescript-eslint";
+import tseslint from "typescript-eslint";
 
-export default [
+// todo: revisit https://typescript-eslint.io/users/configs
+// also see: https://typescript-eslint.io/getting-started/
+// you need to also use the setup function: https://typescript-eslint.io/packages/typescript-eslint/#config
+export default tseslint.config(
     // order matters, as subsequent configs will override
     eslint.configs.recommended,
-    ...typescript.configs.recommended,
+    ...tseslint.configs.recommended,
     ...svelte.configs["flat/prettier"],
     prettier,
     {
@@ -18,8 +23,8 @@ export default [
         files: ["**/*.svelte"],
         parser: svelteParser,
         // previously `parserOptions` also needed `extraFileExtensions: [".svelte"]`, but this seems to work without now
-        parserOptions: { parser: typescript.parser },
+        parserOptions: { parser: tseslint.parser },
         // previously `settings: { compileOptions: { postcss: { configFilePath: "./postcss.config.cjs", },`
         // but this also no longer seems to be needed
     },
-];
+);

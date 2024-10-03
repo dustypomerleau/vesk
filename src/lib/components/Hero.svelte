25872511b5ff5cd9454dd7d5b028424fbc7af4c7
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2024-10-04 09:24:55 +1000

    chore(Cpd): remove banner

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -6,7 +6,7 @@
     id="hero"
     class="flex h-screen flex-col items-center justify-start bg-blue-10 bg-hero bg-cover bg-top bg-blend-multiply"
 >
-    <Cpd />
+    <!-- <Cpd /> -->
     <h1
         class="w-screen px-7 pt-24 text-center text-6xl font-light text-white sm:pt-48 md:px-32 lg:w-2/3"
     >

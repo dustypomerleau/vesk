bce18498bac6f4fc74e2ae912d7586e7a058e197
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-09-14 16:24:09 +1000

    feat(nav): initial burger menu

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -9,18 +9,33 @@
         </div>
     </a>
 </header>
+<nav class="nav-container">
+    <input class="checkbox" type="checkbox" name="" id="" />
+    <div class="hamburger-lines">
+        <span class="line line1"></span>
+        <span class="line line2"></span>
+        <span class="line line3"></span>
+    </div>
+    <div class="menu-items">
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/#location">Location</a></li>
+            <li><a href="/#doctors">Our Doctors</a></li>
+            <li><a href="/#info">Patient Information</a></li>
+            <li><a href="/refer">Refer</a></li>
+            <li><a href="/#contact">Contact</a></li>
+        </ul>
+    </div>
+</nav>
 
 <style>
     .header {
         display: flex;
-        flex-wrap: wrap;
         justify-content: space-between;
         align-items: center;
 
         background-color: var(--blue-10);
-        column-gap: var(--space-8);
-        padding-block: var(--space-4);
-        padding-inline: var(--space-4);
+        padding: var(--space-4);
 
         @media (width >= 40rem) {
             padding-inline: var(--space-8);
@@ -36,5 +51,124 @@
         @media (width >= 40rem) {
             width: calc(var(--space-4) * 15);
         }
+    }
+
+    nav {
+        overflow-x: hidden;
+    }
+
+    .nav-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        align-items: stretch;
+
+        position: absolute;
+        right: 0;
+        width: 100%;
+
+        @media (width >= 40rem) {
+            width: calc(var(--space-4) * 20);
+        }
+
+        .checkbox {
+            position: absolute;
+            display: block;
+            height: 32px;
+            width: 32px;
+            top: 15px;
+            right: 10px;
+            z-index: 5;
+            opacity: 0;
+            cursor: pointer;
+        }
+
+        .hamburger-lines {
+            display: block;
+            /* this exact height is needed to make the lines cross at their middle, and needs to be adjusted if you adjust the height of .line */
+            height: 24.5px;
+            width: 32px;
+            position: absolute;
+            top: 26px;
+            right: 20px;
+            z-index: 2;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+
+            .line {
+                display: block;
+                height: 2.7px;
+                width: 100%;
+                border-radius: 5px;
+                background: var(--gray-4);
+            }
+
+            .line1 {
+                transform-origin: 0% 0%;
+                transition: transform 0.3s ease-in-out;
+            }
+
+            .line2 {
+                transition: transform 0.1s ease-in-out;
+            }
+
+            .line3 {
+                transform-origin: 0% 100%;
+                transition: transform 0.3s ease-in-out;
+            }
+        }
+
+        input[type="checkbox"]:checked ~ .hamburger-lines .line1 {
+            transform: rotate(45deg);
+        }
+
+        input[type="checkbox"]:checked ~ .hamburger-lines .line2 {
+            transform: scaleY(0);
+        }
+
+        input[type="checkbox"]:checked ~ .hamburger-lines .line3 {
+            transform: rotate(-45deg);
+        }
+
+        input[type="checkbox"]:checked ~ .menu-items {
+            transform: translateX(0);
+        }
+
+        li {
+            display: flex;
+            justify-content: stretch;
+            align-items: stretch;
+
+            list-style: none;
+
+            a {
+                flex-grow: 1;
+
+                padding-block: var(--space-2);
+                color: white;
+                text-decoration: none;
+
+                &:hover {
+                    font-weight: bolder;
+                }
+            }
+        }
+    }
+
+    .menu-items {
+        display: flex;
+        flex-direction: column;
+
+        margin-block-start: calc(var(--space-4) * 4.5);
+        padding-block-end: var(--space-4);
+
+        border-radius: var(--radius-sm);
+        background-color: var(--blue-10);
+        text-align: center;
+
+        /* note: preventing horizontal scroll with this call to translate requires adding overflow-x: hidden to body in app.css */
+        transform: translate(100vw);
+        transition: transform 0.35s ease-in-out;
     }
 </style>
--- src/routes/info/[slug]/+page.ts
+++ src/routes/info/[slug]/+page.ts
@@ -1,41 +1,9 @@
 import type { PostContent, PostMetadata } from "$lib/types";
 import type { Component } from "svelte";
 
-// params:
-// Parameters of the target page - e.g. for a route like `/blog/[slug]`, a `{ slug: string }` object.
-// bookmark: todo: this is throwing an error:
-// hint: some people have had problems with casing issues, and you're using capital Content
-// but in this case, the problem is that vite is trying to import a markdown file you didn't create?
-// where is `preload.js.map.md` coming from?
-//
-// Error: Unknown variable dynamic import: ../preload.js.map.md
-//     at /Users/dn/me/vesk/vite/dynamic-import-helper.js:10:9
-//     at new Promise (<anonymous>)
-//     at __vite_ssr_export_default__ (/Users/dn/me/vesk/vite/dynamic-import-helper.js:6:10)
-//     at load (/Users/dn/me/vesk/src/routes/info/[slug]/+page.ts:8:39)
-//     at load_data (/Users/dn/me/vesk/node_modules/.pnpm/@sveltejs+kit@2.21.5_@sveltejs+vite-plugin-svelte@5.1.0_svelte@5.34.5_vite@6.3.5_@types_a83d7c8fdb834413a46df747fb39d549/node_modules/@sveltejs/kit/src/runtime/server/page/load_data.js:204:43)
-//     at process.processTicksAndRejections (node:internal/process/task_queues:105:5)
-//     at async eval (/Users/dn/me/vesk/node_modules/.pnpm/@sveltejs+kit@2.21.5_@sveltejs+vite-plugin-svelte@5.1.0_svelte@5.34.5_vite@6.3.5_@types_a83d7c8fdb834413a46df747fb39d549/node_modules/@sveltejs/kit/src/runtime/server/page/index.js:183:13)
-// Error: Unknown variable dynamic import: ../plugins.js.map.md
-//     at /Users/dn/me/vesk/vite/dynamic-import-helper.js:10:9
-//     at new Promise (<anonymous>)
-//     at __vite_ssr_export_default__ (/Users/dn/me/vesk/vite/dynamic-import-helper.js:6:10)
-//     at load (/Users/dn/me/vesk/src/routes/info/[slug]/+page.ts:8:39)
-//     at load_data (/Users/dn/me/vesk/node_modules/.pnpm/@sveltejs+kit@2.21.5_@sveltejs+vite-plugin-svelte@5.1.0_svelte@5.34.5_vite@6.3.5_@types_a83d7c8fdb834413a46df747fb39d549/node_modules/@sveltejs/kit/src/runtime/server/page/load_data.js:204:43)
-//     at process.processTicksAndRejections (node:internal/process/task_queues:105:5)
-//     at async eval (/Users/dn/me/vesk/node_modules/.pnpm/@sveltejs+kit@2.21.5_@sveltejs+vite-plugin-svelte@5.1.0_svelte@5.34.5_vite@6.3.5_@types_a83d7c8fdb834413a46df747fb39d549/node_modules/@sveltejs/kit/src/runtime/server/page/index.js:183:13)
-//
-export async function load({
-  params,
-}: {
-  params: { slug: string };
-}): Promise<PostContent> {
-  const {
-    metadata: meta,
-    default: Content,
-  }: { metadata: PostMetadata; default: Component } = await import(
-    `../${params.slug}.md`
-  );
+export async function load({ params }: { params: { slug: string } }): Promise<PostContent> {
+    const { metadata: meta, default: Content }: { metadata: PostMetadata; default: Component } =
+        await import(`../${params.slug}.md`);
 
-  return { meta, Content };
+    return { meta, Content };
 }

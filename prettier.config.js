c4d0fa0aab63c464117a0303aea929e31a3083b2
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2025-10-14 16:23:31 +1100

    chore(fmt): format svg with prettier html

--- prettier.config.js
+++ prettier.config.js
@@ -2,6 +2,7 @@ const config = {
     overrides: [
         { files: "*.md", options: { tabWidth: 2 } },
         { files: "*.svelte", options: { parser: "svelte" } },
+        { files: "*.svg", options: { parser: "html" } },
     ],
     plugins: ["@prettier/plugin-oxc", "prettier-plugin-svelte"],
     printWidth: 100,

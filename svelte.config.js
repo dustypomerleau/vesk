57959277e81a5b63b32ded18252e0cb74d20ac38
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2026-08-20 08:58:19 +1000

    feat(blog): use `rehype-class-names` to format CMS blog images

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,20 @@
 import adapter from "@sveltejs/adapter-netlify";
 import { mdsvex } from "mdsvex";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import rehypeClassNames from "rehype-class-names";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     extensions: [".md", ".svelte"],
     kit: { adapter: adapter(), prerender: { handleUnseenRoutes: "warn" } },
-    // adding `".md"` to mdsvex extensions puts `{ metadata }` into the exported object!
-    preprocess: [vitePreprocess(), mdsvex({ extensions: [".md"] })],
+    preprocess: [
+        vitePreprocess(),
+        mdsvex({
+            // adding `".md"` to mdsvex extensions puts `{ metadata }` into the exported object!
+            extensions: [".md"],
+            rehypePlugins: [[rehypeClassNames, { img: "rehype-img" }]],
+        }),
+    ],
 };
 
 export default config;

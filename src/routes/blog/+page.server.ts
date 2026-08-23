807e926b354da1c8895a1b4fdbb14d329e6cbad7
Dusty Pomerleau <dustypomerleau@users.noreply.github.com>
2026-08-23 15:30:38 +1000

    chore: clean up comments in blog and info server files

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,14 +1,6 @@
-// import type { PostPath } from "$lib/types.js";
 import { getBlogPosts } from "$lib/utils";
-// import type { PageLoadEvent } from "./$types";
 
-export const load =
-    // async ({ fetch }: PageLoadEvent): Promise<{ posts: Array<PostPath> }> => {
-    // const response = await fetch("/api/blog");
-    // const posts = await response.json();
-
-    // For now, remove the API endpoint from the loop and directly load the data on the server.
-    async () => {
-        const posts = await getBlogPosts();
-        return { posts };
-    };
+export const load = async () => {
+    const posts = await getBlogPosts();
+    return { posts };
+};
